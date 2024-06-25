53068694e06554b5b7f404fe72f8369c5bccb6b4
Daniel Beardsley <daniel@ifixit.com>
2024-06-24 22:56:39 -0700

    Drop helmet, this isn't a website

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
 import express, { Express, Request, Response } from 'express';
-import helmet from 'helmet';
 import compression from 'compression';
 import compressFilter from './utils/compressFilter.util';
 import config from './config/config';
 
 const app: Express = express();
-
-// Helmet is used to secure this app by configuring the http-header
-app.use(helmet());
 
 // Compression is used to reduce the size of the response body
 app.use(compression({ filter: compressFilter }));
