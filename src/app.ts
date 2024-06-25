18ccff21238c89b3fbacc2fb964e9de34559cfa6
Daniel Beardsley <daniel@ifixit.com>
2024-06-24 22:52:40 -0700

    Remove cors

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,10 @@
 import express, { Express, Request, Response } from 'express';
-import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
 import compressFilter from './utils/compressFilter.util';
 import config from './config/config';
 
 const app: Express = express();
-
-app.use(
-  cors({
-    // origin is given a array if we want to have multiple origins later
-    origin: [config.cors_origin],
-    credentials: true,
-  })
-);
 
 // Helmet is used to secure this app by configuring the http-header
 app.use(helmet());
--- src/config/config.ts
+++ src/config/config.ts
@@ -8,7 +8,6 @@ dotenv.config({
 const config = {
   node_env: process.env.NODE_ENV,
   port: process.env.PORT,
-  cors_origin: process.env.CORS_ORIGIN,
 };
 
 export default config;
